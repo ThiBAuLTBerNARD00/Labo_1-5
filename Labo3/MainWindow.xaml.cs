73524605116f413d26b6704e1cae48128cfb3e9c
Th <thibault.bernard@student.hepl.be>
2023-03-08 16:23:11 +0100

    labo 3 bien avancé

--- Labo3/MainWindow.xaml.cs
+++ Labo3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaLibrairieForme;
 
 namespace Labo3
 {
@@ -20,9 +22,53 @@ namespace Labo3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ObservableCollection<Forme> ListeForme = new();
         public MainWindow()
         {
             InitializeComponent();
+            ListViewForm.ItemsSource = ListeForme;
         }
+
+        private void ComboBoxForm_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!IsInitialized) return;
+
+            ComboBoxItem item = (ComboBoxItem)ComboBoxForm.SelectedItem;
+            if (item is not null)
+            {
+                if (item.Content.Equals("Rectangle"))
+                {
+                    LabelLongueur.Content = "Longueur";
+                    LabelLargeur.Visibility = Visibility.Visible;
+                    TextBoxLargeur.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    if (item.Content.Equals("Carre"))
+                    {
+                        LabelLongueur.Content = "Côté";
+                    }
+                    else
+                    {
+                        LabelLongueur.Content = "Rayon";
+                    }
+                    LabelLargeur.Visibility = Visibility.Hidden;
+                    TextBoxLargeur.Visibility = Visibility.Hidden;
+                }
+            }
+
+        }
+        private void ListViewForm_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListViewForm.SelectedItem is null)
+            {
+                LabelFormInfos.Content = "AUCUNE FORME SELECTIONNEE !";
+            }
+            else
+            {
+                LabelFormInfos.Content = ListViewForm.SelectedItem.GetType().Name + " : " + ListViewForm.SelectedItem.ToString();
+            }
+        }
+
     }
 }
